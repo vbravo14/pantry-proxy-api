dd75aff593bb9497c0dfc999cb6e999bc818be53
vbravo14 <bravo.victor0810@gmail.com>
2026-05-02 20:08:38 -0400

    styles and logic update

--- public/script.js
+++ public/script.js
@@ -31,20 +31,22 @@ async function fetchQuestions() {
 // Load each question to UI
 function loadQuestion() {
           const question = questions[currentQuestion];
-          questionText.innerText = question.question;
+          questionText.innerText = decodeHTML(question.question);
           questionNumber.innerText = `Question ${currentQuestion + 1}`;
 
           // Reset choices
           choicesContainer.innerText = '';
-          // nextBtn.disabled = true;
+          nextBtn.disabled = true;
 
           //Prepare choices
           const choices = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
           choices.forEach(choice => {
+                    choice = choice.trim();
                     const button = document.createElement('button');
                     button.type = 'button';
                     button.classList.add('choice');
-                    button.innerText = choice;
+                    button.innerText = decodeHTML(choice);
+                    button.onclick = () => checkAnswer(choice, question.correct_answer.trim())
                     choicesContainer.appendChild(button);
           });
 }
@@ -54,6 +56,30 @@ nextBtn.addEventListener('click', () => {
           currentQuestion++;
           loadQuestion();
 })
+
+// Ensure our special characters are decoded
+function decodeHTML(html) {
+         const txt = document.createElement('div');
+         txt.innerHTML = html;
+         return txt.textContent;
+}
+
+// Check if answer is correct
+function checkAnswer(selectedAnswer, correctAnswer) {
+          console.log(selectedAnswer, correctAnswer)
+
+          const choices = document.querySelectorAll('.choice');
+          choices.forEach(choice => {
+                    if (choice.innerText === decodeHTML(correctAnswer)) {
+                              choice.classList.add('correct');
+                    } else {
+                              choice.classList.add('wrong');
+                    }
+                    choice.disabled = true;
+          });
+
+          nextBtn.disabled = false;
+}
 
 // Startup
 fetchQuestions();
