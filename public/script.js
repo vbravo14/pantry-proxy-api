1ec010126212ba5ee934ec32e216747dea4923ff
vbravo14 <bravo.victor0810@gmail.com>
2026-06-10 00:47:46 -0400

    updated time and score function

--- public/script.js
+++ public/script.js
@@ -14,6 +14,10 @@ const playAgainBtn = document.getElementById('play-again-btn');
 
 let currentQuestion = 0;
 let questions = [];
+let totalScore = 0;
+let timerInterval;
+let startTime;
+const totalTime = 10000;
 
 // Fetch Questions
 async function fetchQuestions() {
@@ -49,6 +53,9 @@ function loadQuestion() {
                     button.onclick = () => checkAnswer(choice, question.correct_answer.trim())
                     choicesContainer.appendChild(button);
           });
+
+          resetTimer();
+          startTimer();
 }
 
 // Load next question
@@ -64,9 +71,46 @@ function decodeHTML(html) {
          return txt.textContent;
 }
 
+// Start timer for question
+function startTimer() {
+          startTime = Date.now();
+          let timeLeft = totalTime;
+          updateTimerDisplay(timeLeft);
+
+          timerInterval = setInterval(() => {
+                    const elapsedTime = Date.now() - startTime;
+                    timeLeft = totalTime - elapsedTime;
+
+                    if (timeLeft <= 0) {
+                              clearInterval(timerInterval);
+                              timeLeft = 0;
+                              updateTimerDisplay(timeLeft);
+                              disableChoices();
+                              nextBtn.disabled = false;
+                              const correctAnswer = questions[currentQuestion].correct_answer;
+                              highlightCorrectAnswer(correctAnswer);
+                    } else {
+                              updateTimerDisplay(timeLeft);
+                    }
+          }, 50);
+}
+
+// Reset timer to 10s
+function resetTimer() {
+          clearInterval(timerInterval);
+          updateTimerDisplay(totalTime);
+}
+
+// Update timer UI
+function updateTimerDisplay(timeLeft) {
+          const seconds = (timeLeft / 1000).toFixed(2);
+          timerDisplay.innerText = seconds;
+}
+
 // Check if answer is correct
 function checkAnswer(selectedAnswer, correctAnswer) {
-          console.log(selectedAnswer, correctAnswer)
+          clearInterval(timerInterval);
+          disableChoices();
 
           const choices = document.querySelectorAll('.choice');
           choices.forEach(choice => {
@@ -78,7 +122,33 @@ function checkAnswer(selectedAnswer, correctAnswer) {
                     choice.disabled = true;
           });
 
+          if(selectedAnswer === correctAnswer) {
+                    const elapsedTime = Date.now() - startTime;
+                    const timeLeft = totalTime - elapsedTime;
+                    const weightedScore = Math.floor((timeLeft / totalTime) * 1000);
+                    totalScore += weightedScore;
+                    console.log(weightedScore, 'total', totalScore);
+          }
+
           nextBtn.disabled = false;
+}
+
+// Disable choice when time expires or choice selected
+function disableChoices() {
+          const choices = document.querySelectorAll('.choice');
+          choices.forEach(choice => {
+                    choice.disabled = true;
+          });
+}
+
+// Highlight correct answer
+function highlightCorrectAnswer(correctAnswer) {
+          const choices = document.querySelectorAll('.choice');
+          choices.forEach(choice => {
+                    if(choice.innerText === decodeHTML(correctAnswer)) {
+                              choice.classList.add('correct');
+                    }
+          });
 }
 
 // Startup
